cd4cbb1a174e759216fc6a3656107f9d9720a8b7
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-05 18:56:36 +0300

    Ajout de model

--- web/auto/models.py
+++ web/auto/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class index(models.Model):
+    achat_carte_credit = models.BooleanField(default=False)
+    montant = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    heure_envoi = models.DateTimeField(auto_now_add=True)
