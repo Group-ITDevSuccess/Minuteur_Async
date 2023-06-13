078eb65f92c60f3c8779ac43b91570bc113c7d6c
muriel.raharison <muriel.raharison@inviso-group.com>
2023-06-13 09:59:05 +0300

    Mise en place de teste 00:00:00:00

--- func/calculate_next_thursday.py
+++ func/calculate_next_thursday.py
@@ -5,6 +5,6 @@ def calculate_next_thursday():
     date_heur_actuel = datetime.datetime.now()
     jours = (1 - date_heur_actuel.weekday() + 7) % 7
     date_heur_prochaine = date_heur_actuel + datetime.timedelta(days=jours)
-    date_heur_prochaine = date_heur_prochaine.replace(hour=9, minute=0, second=0, microsecond=0)
+    date_heur_prochaine = date_heur_prochaine.replace(hour=23, minute=59, second=59, microsecond=0)
     return date_heur_prochaine
 
--- func/calculate_time_remaining.py
+++ func/calculate_time_remaining.py
@@ -3,8 +3,8 @@ import datetime
 
 def calculate_time_remaining():
     date_heur_actuel = datetime.datetime.now()
-    jours = (0 - date_heur_actuel.weekday() + 7) % 7
-    date_heur_prochaine = date_heur_actuel.replace(hour=15, minute=35, second=0, microsecond=0)
+    jours = (1 - date_heur_actuel.weekday() + 7) % 7
+    date_heur_prochaine = date_heur_actuel.replace(hour=23, minute=59, second=59, microsecond=0)
 
     if date_heur_actuel >= date_heur_prochaine:
         jours += 7
