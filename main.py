7e4e23196e0e6b60844f5274da0c40060cc70e65
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-21 17:05:38 +0300

    Ajout de trie

--- main.py
+++ main.py
@@ -53,7 +53,6 @@ def create_historique_table():
         # Handle the error if the table creation fails
         messagebox.showerror("Error", f"An error occurred while creating the 'historique' table: {str(e)}")
 
-
 def data_sent_email():
     try:
         conn = sqlite3.connect('./DB_TEST.sqlite3')
@@ -117,19 +116,16 @@ def format_time(days, hours, minutes, seconds):
     return f"{days} jours, {hours:02d} heures, {minutes:02d} minutes, {seconds:02d} secondes"
 
 
-def update_history_table(order_by=None):
+def update_history_table():
     try:
         conn = sqlite3.connect('./DB_TEST.sqlite3')
         cursor = conn.cursor()
 
-        # Requête de base pour récupérer les données de l'historique
-        query = "SELECT email, data, date, time, status FROM historique"
-
-        # Ajouter la clause ORDER BY si une colonne de tri est spécifiée
-        if order_by:
-            query += f" ORDER BY {order_by}"
-
-        cursor.execute(query)
+        # Fetch data from the 'historique' table in descending order based on 'date' and 'time'
+        cursor.execute("""
+            SELECT email, data, date, time, status FROM historique
+            ORDER BY date DESC, time DESC
+        """)
         rows = cursor.fetchall()
 
         history_tree.delete(*history_tree.get_children())
@@ -141,12 +137,6 @@ def update_history_table(order_by=None):
     except sqlite3.Error as e:
         messagebox.showerror("Error",
                              f"An error occurred while fetching data from the 'historique' table: {str(e)}")
-
-
-def sort_column(col):
-    # Appeler la fonction de mise à jour du tableau avec la colonne sur laquelle trier
-    update_history_table(order_by=col)
-
 
 def update_label_periodically():
     update_time_remaining_label()
@@ -203,20 +193,18 @@ if __name__ == "__main__":
     execute_button = ttk.Button(content_frame, text="Exécuter le script", command=query_thread, style='Custom.TButton')
     execute_button.pack(pady=10)
 
-    # Créer une Treeview widget pour afficher la table historique
+    # Create a Treeview widget to display the history table
     history_tree = ttk.Treeview(content_frame, columns=("Email", "Data", "Date", "Time", "Statut"), show="headings", style='Custom.Treeview')
-    history_tree.heading("Email", text="Email", anchor=tk.CENTER, command=lambda: sort_column("email"))
-    history_tree.heading("Data", text="Data", anchor=tk.CENTER, command=lambda: sort_column("data"))
-    history_tree.heading("Date", text="Date", anchor=tk.CENTER, command=lambda: sort_column("date"))
-    history_tree.heading("Time", text="Time", anchor=tk.CENTER, command=lambda: sort_column("time"))
-    history_tree.heading("Statut", text="Statut", anchor=tk.CENTER, command=lambda: sort_column("status"))
+    history_tree.heading("Email", text="Email", anchor=tk.CENTER)
+    history_tree.heading("Data", text="Data", anchor=tk.CENTER)
+    history_tree.heading("Date", text="Date", anchor=tk.CENTER)
+    history_tree.heading("Time", text="Time", anchor=tk.CENTER)
+    history_tree.heading("Statut", text="Statut", anchor=tk.CENTER)
     history_tree.pack(fill=tk.BOTH, expand=True, padx=10, pady=5)
-
-    # Appeler la fonction pour mettre à jour la table et activer le tri initial (sans tri)
-    update_history_table()
 
     # Call the function to update the label and history table periodically
     update_label_periodically()
+
 
     # Call the function to update the history table periodically
     update_history_table()
