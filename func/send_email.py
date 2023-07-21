bd3a02faa7d31e602e4f72d46049aa491d46e2da
ITDevSuccess <raharisontsidiany@gmail.com>
2023-07-21 11:29:56 +0300

    Mise en place de date

--- func/send_email.py
+++ func/send_email.py
@@ -3,16 +3,15 @@ import sqlite3
 import datetime
 from email.message import EmailMessage
 
-
 def send_email_with_attachment(objet, filename, recipients, smtp):
     # Vérifiez si les variables d'environnement sont définies
-    if smtp['username'] is None or smtp['password'] is None or smtp['port'] is None:
+    if smtp.get('username') is None or smtp.get('password') is None or smtp.get('port') is None:
         raise ValueError("Les variables d'environnement doivent être définies.")
 
     message = EmailMessage()
     message["Subject"] = f"Données de {objet}"
-    message["From"] = smtp['username']
-    message["To"] = recipients
+    message["From"] = smtp.get('username')
+    message["To"] = ", ".join(recipients)  # Concaténer les adresses e-mail avec une virgule
 
     message.set_content("Veuillez trouver ci-joint le fichier Excel contenant les résultats de la requête SQL.")
 
@@ -20,9 +19,9 @@ def send_email_with_attachment(objet, filename, recipients, smtp):
         content = file.read()
         message.add_attachment(content, maintype="application", subtype="octet-stream", filename=filename)
 
-    with smtplib.SMTP(smtp['server'], int(smtp['port'])) as server:
+    with smtplib.SMTP(smtp.get('server'), int(smtp.get('port'))) as server:
         server.starttls()
-        server.login(smtp['username'], smtp['password'])
+        server.login(smtp.get('username'), smtp.get('password'))
         server.send_message(message)
         print(f"Message sent to {recipients}")
     
@@ -31,6 +30,6 @@ def send_email_with_attachment(objet, filename, recipients, smtp):
     cursor.execute("""
         INSERT INTO historique (email, data, date, time)
         VALUES (?, ?, ?, ?)
-        """, (recipients, filename, datetime.datetime.now().date(), datetime.datetime.now().time()))
+        """, (", ".join(recipients), filename, datetime.datetime.now().date(), datetime.datetime.now().time()))
     conn.commit()
     conn.close()
